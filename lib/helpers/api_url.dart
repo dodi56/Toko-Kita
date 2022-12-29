31acaaa7b5d68b3d5ebeb9ba3ea02f290c87d413
dodi56 <dodisuhendra1956@gmail.com>
2022-12-29 20:59:21 +0700

    CodeIgniter-4/Flutter

---
+++ lib/helpers/api_url.dart
@@ -0,0 +1,21 @@
+class ApiUrl {
+  //ip tetring asus
+  static const String baseUrl = 'http://172.20.10.4/toko-api/public';
+
+  static const String registrasi = baseUrl + '/registrasi';
+  static const String login = baseUrl + '/login';
+  static const String listProduk = baseUrl + '/produk';
+  static const String createProduk = baseUrl + '/produk';
+
+  static String updateProduk(int id) {
+    return baseUrl + '/produk/' + id.toString() + '/update';
+  }
+
+  static String showProduk(int id) {
+    return baseUrl + '/produk/' + id.toString();
+  }
+
+  static String deleteProduk(int id) {
+    return baseUrl + '/produk/' + id.toString();
+  }
+}
